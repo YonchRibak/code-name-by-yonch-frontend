a2c0ca919286684a6da9d0db20aaac51bd1f07be
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-06-30 17:04:12 +0300

    base url configs

--- src/Utils/appConfig.ts
+++ src/Utils/appConfig.ts
@@ -1,6 +1,6 @@
 class AppConfig {
-  public readonly baseUrl = "http://localhost:4000";
-  public readonly wordBankUrl = "http://localhost:4000/api/words";
+  public readonly baseUrl = import.meta.env.VITE_API_URL;
+  public readonly wordBankUrl = this.baseUrl + "/api/words";
   public WikiUrl(lang: string): string {
     return `https://${lang}.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=extracts&exsentences=1&exintro&explaintext&generator=random&grnnamespace=0&grnlimit=20`;
   }
