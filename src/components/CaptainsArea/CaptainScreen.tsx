111f4e142598afa417bee72f14e335dada39f0d6
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-07-01 17:28:07 +0300

    small style modifications

--- src/components/CaptainsArea/CaptainScreen.tsx
+++ src/components/CaptainsArea/CaptainScreen.tsx
@@ -21,7 +21,7 @@ function CaptainScreen(): JSX.Element {
     return (
       <div className="flex flex-col overflow-hidden justify-center items-center gap-3 w-full h-full">
         <LangToggler className="w-1/4" />
-        <div className="text-2xl font-semibold whitespace-pre-line">
+        <div className="text-xl font-semibold whitespace-pre-line">
           {t("captain.landscape")}
         </div>
       </div>
