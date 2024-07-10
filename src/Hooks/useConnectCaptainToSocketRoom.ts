8fffc61565588ccc2e4d4f8f2d916fadee59208c
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-07-10 14:46:10 +0300

    fixed infinite connection at useConnectCaptainToSocketRoom

--- src/Hooks/useConnectCaptainToSocketRoom.ts
+++ src/Hooks/useConnectCaptainToSocketRoom.ts
@@ -1,6 +1,6 @@
 import { socketService } from "@/Services/SocketService";
 import { Session } from "@/components/game-provider";
-import { Dispatch, SetStateAction, useEffect } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef } from "react";
 import { Params } from "react-router-dom";
 
 function useConnectCaptainToSocketRoom(
@@ -8,13 +8,17 @@ function useConnectCaptainToSocketRoom(
   params: Readonly<Params<string>>,
   setSession: Dispatch<SetStateAction<Session>>
 ) {
+  // Use a ref to track if the connection has been made
+  const hasConnected = useRef(false);
   useEffect(() => {
-    if (!socketService.isConnected()) {
+    if (!hasConnected.current) {
       // Connect to the socket server
       socketService.connect();
 
       // Emit a connection message to the server indicating a captain is trying to connect
       socketService.joinRoom(params.gameId, setSession);
+      // Set the ref to true to prevent re-connecting
+      hasConnected.current = true;
     }
 
     // Cleanup function to disconnect from the server when the component unmounts
